1b32bef9f42ae4f0a2e9518d271706046a89677f
lionelsaxer <lionel.saxer@gmail.com>
2024-08-05 23:31:19 +0200

    Update files

--- queries/2_skills_per_role.sql
+++ queries/2_skills_per_role.sql
@@ -15,7 +15,7 @@ WITH filtered_jobs AS (
 
 -- Join with skills tables and count number of job posts for a specific skill
 SELECT
-    job_title_short,
+    job_title_short AS role,
     skills,
     COUNT(skills_job_dim.job_id) AS demand_count
 FROM
@@ -23,4 +23,4 @@ FROM
 INNER JOIN skills_job_dim ON filtered_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 GROUP BY
-    job_title_short, skills
+    role, skills
