ada846113d9cc2dafbd1807b7012bcb730402b93
lionelsaxer <lionel.saxer@gmail.com>
2024-08-07 15:12:31 +0200

    update

--- sql_queries/4_optimal_skills.sql
+++ sql_queries/4_optimal_skills.sql
@@ -8,48 +8,18 @@ WITH filtered_jobs AS (
         NOT job_location = 'Anywhere' AND
         job_schedule_type = 'Full-time' AND
         salary_year_avg IS NOT NULL
-),
-
--- Define CTE for skill demand
-skill_demand AS (
-    SELECT
-        job_title_short AS role,
-        skills_dim.skill_id,
-        skills_dim.skills,
-        COUNT(skills_job_dim.job_id) AS demand_count
-    FROM
-        filtered_jobs
-    INNER JOIN skills_job_dim ON filtered_jobs.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    GROUP BY
-        role, skills_dim.skill_id
-),
-
--- Define CTE for skill salary
-skill_salary AS (
-    SELECT
-        job_title_short AS role,
-        skills_dim.skill_id,
-        ROUND(AVG(salary_year_avg), 0) AS avg_yearly_salary
-    FROM
-        filtered_jobs
-    INNER JOIN skills_job_dim ON filtered_jobs.job_id = skills_job_dim.job_id
-    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-    GROUP BY
-        role, skills_dim.skill_id
 )
 
--- Combine CTEs
 SELECT
-    skill_demand.role,
-    skill_demand.skill_id,
-    skill_demand.skills,
-    demand_count,
-    avg_yearly_salary
+    job_title_short AS role,
+    skills AS skill,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(salary_year_avg), 0) AS avg_yearly_salary
 FROM
-    skill_demand
-INNER JOIN skill_salary ON skill_demand.skill_id = skill_salary.skill_id
-AND skill_demand.role = skill_salary.role
-
+    filtered_jobs
+INNER JOIN skills_job_dim ON filtered_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY
+    role, skills_dim.skill_id
 
 
